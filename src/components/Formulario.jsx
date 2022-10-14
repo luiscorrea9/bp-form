86449312254ec0638a7e5e70736794850ecb83f5
Luis Correa <Luiscorrea874@gmail.com>
2022-10-14 12:03:53 -0500

    Limpieza

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const formulario = () => {
+    return (  );
+}
+ 
+export default formulario;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import formulario from './components/Formulario';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <formulario />
   </React.StrictMode>
 );
 
