1b9706951058501a10ff24cd9b0e12d5bccc5944
Luis Correa <Luiscorrea874@gmail.com>
2022-10-15 18:02:45 -0500

    Avance boton cancelar

--- src/components/formulario.jsx
+++ src/components/formulario.jsx
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {db} from "../firebase"
-import { collection, addDoc, deleteDoc, doc} from 'firebase/firestore';
+import { collection, addDoc, deleteDoc, doc, onSnapshot} from 'firebase/firestore';
 
 const Formulario = () => {
 
@@ -12,6 +12,24 @@ const Formulario = () => {
     const [country, setCountry] = useState('');
     const [location, setLocation] = useState('');
     const [lista, setLista] = useState([]);
+    const [editingMode, setEditingMode] = useState(false); 
+    const[id, setId] = useState('');
+
+    
+
+     
+    useEffect(()=>{
+      const obtenerDatos = async () => {
+          try{
+             await onSnapshot(collection(db, "formulario"), (query) =>{
+               setLista(query.docs.map((doc) => ({...doc.data(),id:doc.id})))
+             })
+          }catch(error){
+              console.log(error);
+          }   
+      } 
+       obtenerDatos();  
+    },[])
 
 
    const saveList = async (e) => {
@@ -53,6 +71,33 @@ const Formulario = () => {
       console.log(error);
     } 
    }
+
+
+  const edit = item =>{
+      setName(item.infoName);
+      setLastname(item.infoLastname); 
+      setAge(item.infoAge);
+      setEmail(item.infoEmail);
+      setPhone(item.infoPhone);
+      setCountry(item.infoCountry);
+      setLocation(item.infoLocation);
+      setId(item.id);
+      setEditingMode(true);
+
+  } 
+
+  const cancel = () =>{
+    setEditingMode(false);
+    setName("");
+    setLastname("");
+    setAge("");
+    setEmail("");
+    setPhone("");
+    setCountry("");
+    setLocation("");
+    setId("");
+    
+  }
    
 
     return (
@@ -61,7 +106,13 @@ const Formulario = () => {
     <hr />
     <div className="flex-row">
       <div className="flex-large">
-        <h2>Add user</h2>
+        <h2>
+          {
+            editingMode ? "Edit user" : "Add user"
+                          
+          }
+        </h2>
+
         <form onSubmit={saveList}>
 
           <input type="text"
@@ -96,28 +147,34 @@ const Formulario = () => {
           className='form-control mb-2'
           placeholder='Country'
           value={country}
-          onChange={(e) => setPhone(e.target.value)}
+          onChange={(e) => setCountry(e.target.value)}
           />
 
           <input type="text"
           className='form-control mb-2'
           placeholder='Location'
           value={location}
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => setLocation(e.target.value)}
           />
 
           <input type="number"
           className='form-control mb-2'
           placeholder='Phone'
           value={phone}
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => setPhone(e.target.value)}
           />
           
-          
+          {
+            editingMode ? 
+            (
+            <><button className=" btn btn-warning btn-lg" type="submit">Editar</button>
+            <button className=" btn btn-dark btn-lg mx-2" onClick={() =>cancel()}>Cancelar</button></> )
+            : <button className=" btn btn-primary btn-lg" type="submit">Add</button>
+          }
           
         
-               
-         <button className=" btn btn-primary btn-block" type="submit">Add</button>
+        
+        
          
         </form>
 
@@ -130,9 +187,10 @@ const Formulario = () => {
               lista.map(item => (
                 <li key={item.id} className="list-group-item">
                   <span className='lead'>{item.infoName}-{item.infoLastname}-{item.infoAge}-{item.infoEmail}-{item.infoCountry}-{item.infoLocation}-{item.infoPhone}</span>
-                  <button className='btn btn-danger btn-sm float-end mx-2'
+                  <button className='btn btn-danger btn-lg float-end mx-2'
                   onClick={() => eliminar(item.id)}>Eliminar</button>
-                  <button className='btn btn-warning btn-sm float-end mx-2'>Editar</button>
+                  <button className='btn btn-warning btn-lg float-end mx-2'
+                  onClick={() => edit(item)}>Editar</button>
                 </li>
               ))
 
